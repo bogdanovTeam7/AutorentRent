0147e320f8c944b70487a3b0c6aa1f53c4090a2d
bogdanovTeam7 <bogdanov@t-email.hu>
2022-11-12 19:41:20 +0100

    init

---
+++ Tom-SB-Rest-AutorentRent/src/main/java/pti/sb_rest_autorentrent/TomSbRestAutorentRentApplication.java
@@ -0,0 +1,13 @@
+package pti.sb_rest_autorentrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomSbRestAutorentRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomSbRestAutorentRentApplication.class, args);
+	}
+
+}
---
+++ Tom-SB-Rest-AutorentRent/src/test/java/pti/sb_rest_autorentrent/TomSbRestAutorentRentApplicationTests.java
@@ -0,0 +1,13 @@
+package pti.sb_rest_autorentrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomSbRestAutorentRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
